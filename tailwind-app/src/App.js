bf31a994aa9815bdc8856926a96324d25fbf4f65
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-26 17:05:52 +0200

    Import Navbar component to App.js

--- tailwind-app/src/App.js
+++ tailwind-app/src/App.js
@@ -2,11 +2,11 @@ import Navbar from "./components/Navbar";
 
 
 function App() {
-  return (
-    <div className="">
-  <Navbar/>
-    </div>
-  );
+    return (
+        <div className="">
+            <Navbar/>
+        </div>
+    );
 }
 
 export default App;
