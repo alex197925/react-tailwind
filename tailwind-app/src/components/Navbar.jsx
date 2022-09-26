039c0af74d7b97f00cf8c385dcd73404ce7106d1
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-26 17:45:12 +0200

    Add icons

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import {BsPerson} from 'react-icons/bs';
 import {BiSearch} from 'react-icons/bi';
 import {AiOutlineMenu} from 'react-icons/ai';
+import {FaFacebook, FaYoutube, FaPinterest, FaInstagram, FaTwitter} from 'react-icons/fa';
+
+
+
+
 
 const Navbar = () => {
     return (
         <div className="flex justify-between items-center h-20 px-2">
-            <div className=""><h1>BEACHES.</h1></div>
+            <div><h1>BEACHES.</h1></div>
             <div>
                 <ul className="hidden md:flex">
                     <li>Home</li>
@@ -22,6 +27,27 @@ const Navbar = () => {
             </div>
             <div className="md:hidden">
                 <AiOutlineMenu size={20}/>
+            </div>
+            <div>
+                <ul>
+                    <h1>BEACHES.</h1>
+                    <li>Home</li>
+                    <li>Destinations</li>
+                    <li>Travel</li>
+                    <li>View</li>
+                    <li>Book</li>
+                    <div>
+                        <button>Search</button>
+                        <button>Account</button>
+                    </div>
+                    <div>
+                        <FaFacebook className="icon"/>
+                        <FaTwitter className="icon"/>
+                        <FaInstagram className="icon"/>
+                        <FaPinterest className="icon"/>
+                        <FaYoutube className="icon"/>
+                    </div>
+                </ul>
             </div>
         </div>
     );
