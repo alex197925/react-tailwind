265816a0f8c59fe87f4edb042a9e3d9806a9bab8
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-26 18:10:43 +0200

    Add style for hamburger menu

--- tailwind-app/src/components/Navbar.jsx
+++ tailwind-app/src/components/Navbar.jsx
@@ -5,9 +5,6 @@ import {AiOutlineMenu} from 'react-icons/ai';
 import {FaFacebook, FaYoutube, FaPinterest, FaInstagram, FaTwitter} from 'react-icons/fa';
 
 
-
-
-
 const Navbar = () => {
     return (
         <div className="flex justify-between items-center h-20 px-2">
@@ -25,22 +22,26 @@ const Navbar = () => {
                 <BiSearch size={20}/>
                 <BsPerson size={20}/>
             </div>
+
+            {/*Hamburger*/}
             <div className="md:hidden">
                 <AiOutlineMenu size={20}/>
             </div>
-            <div>
+
+            {/*Mobile menu dropdown*/}
+            <div className="absolute left-[-100%] top-0 w-full bg-gray-100/90 px-4 py-7 flex flex-col">
                 <ul>
                     <h1>BEACHES.</h1>
-                    <li>Home</li>
-                    <li>Destinations</li>
-                    <li>Travel</li>
-                    <li>View</li>
-                    <li>Book</li>
+                    <li className="border-b">Home</li>
+                    <li className="border-b">Destinations</li>
+                    <li className="border-b">Travel</li>
+                    <li className="border-b">View</li>
+                    <li className="border-b">Book</li>
                     <div>
                         <button>Search</button>
                         <button>Account</button>
                     </div>
-                    <div>
+                    <div className="flex justify-around py-4 px-4">
                         <FaFacebook className="icon"/>
                         <FaTwitter className="icon"/>
                         <FaInstagram className="icon"/>
