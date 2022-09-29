44b3188665b93478c44d8eee60e37ff8b7b64304
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-29 17:49:03 +0200

    Add Destination component, imported 5 images

--- tailwind-app/src/App.js
+++ tailwind-app/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Destinations from "./components/Destinations";
 
 function App() {
     return (
         <div className="">
             <Navbar/>
             <Hero/>
+            <Destinations/>
         </div>
     );
 }
---
+++ tailwind-app/src/components/Destinations.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import BoraBora from '../assets/borabora.jpg';
+import BoraBora2 from '../assets/borabora2.jpg';
+import KeyWest from '../assets/keywest.jpg';
+import Maldives from '../assets/maldives.jpg';
+import Maldives2 from '../assets/maldives2.jpg';
+
+const Destinations = () => {
+    return (
+        <div className="max-w-[1240px] mx-auto py-16 px-4 text-center">
+            <h1>All-Inclusive Resorts</h1>
+            <p>On the Caribbean's Best Beaches</p>
+        {/*    Use Grid for to place images*/}
+            <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md: gap-4">
+                {/*This classes making images the same size*/}
+                <img className="w-full h-full object-cover" src={BoraBora} alt="bora bora"/>
+                <img className="w-full h-full object-cover" src={BoraBora2} alt="bora bora"/>
+                <img className="w-full h-full object-cover" src={KeyWest} alt="key west"/>
+                <img className="w-full h-full object-cover" src={Maldives} alt="maldives"/>
+                <img className="w-full h-full object-cover" src={Maldives2} alt="maldives"/>
+            </div>
+        </div>
+    );
+};
+
+export default Destinations;
