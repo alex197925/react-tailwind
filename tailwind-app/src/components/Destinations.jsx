bab94eafd62398bacbc7a42e769a313535632072
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-29 18:01:45 +0200

    Add style to images with grid col-span

--- tailwind-app/src/components/Destinations.jsx
+++ tailwind-app/src/components/Destinations.jsx
@@ -13,7 +13,8 @@ const Destinations = () => {
         {/*    Use Grid for to place images*/}
             <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md: gap-4">
                 {/*This classes making images the same size*/}
-                <img className="w-full h-full object-cover" src={BoraBora} alt="bora bora"/>
+                {/* ON the first img we add more style for  different screen size*/}
+                <img className="w-full h-full object-cover col-span-2 md:col-span-3 row-span-2" src={BoraBora} alt="bora bora"/>
                 <img className="w-full h-full object-cover" src={BoraBora2} alt="bora bora"/>
                 <img className="w-full h-full object-cover" src={KeyWest} alt="key west"/>
                 <img className="w-full h-full object-cover" src={Maldives} alt="maldives"/>
