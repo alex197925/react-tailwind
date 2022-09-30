4bd09e0c4656fdbeae7c061b76c0b10fca41588d
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-30 15:00:28 +0200

    Created new SelectsCard component, passing data(props) from Selects to  SelectsCard component

--- tailwind-app/src/components/Selects.jsx
+++ tailwind-app/src/components/Selects.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import Island from '../assets/island.jpg';
-import Island2 from '../assets/island2.jpg';
-import Island3 from '../assets/island3.jpg';
-import Island4 from '../assets/island4.jpg';
-import Island5 from '../assets/island5.jpg';
-import Island6 from '../assets/island6.jpg';
+import BoraBora from '../assets/borabora.jpg';
+import BoraBora2 from '../assets/borabora2.jpg';
+import KeyWest from '../assets/keywest.jpg';
+import Maldives from '../assets/maldives.jpg';
+import Maldives2 from '../assets/maldives2.jpg';
+import SelectsCard from "./SelectsCard";
 
 
 const Selects = () => {
     return (
-        <div className="max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3">
-            <div className="relative">
-                <img src={Island} alt=""/>
-                <img src={Island2} alt=""/>
-                <img src={Island3} alt=""/>
-                <img src={Island4} alt=""/>
-                <img src={Island5} alt=""/>
-                <img src={Island6} alt=""/>
-            </div>
+        <div className="'max-w-[1240px] mx-auto px-4 py-16 grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
+            <SelectsCard bg={BoraBora} text='BoraBora'/>
+            <SelectsCard bg={BoraBora2} text='Maldives'/>
+            <SelectsCard bg={KeyWest} text='Key West'/>
+            <SelectsCard bg={Maldives} text='Thailand'/>
+            <SelectsCard bg={Maldives2} text='Malaysia'/>
+            <SelectsCard bg={BoraBora2} text='Maldives'/>
         </div>
     );
 };
