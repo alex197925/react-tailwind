e6908b5fbd1e5ae49ec1834bf45eadca377b016f
tolok oleksandr <tolok.oleksandr@gmail.com>
2022-09-30 13:55:12 +0200

    Add Booking part

--- tailwind-app/src/components/Search.jsx
+++ tailwind-app/src/components/Search.jsx
@@ -5,9 +5,10 @@ import {MdTravelExplore} from 'react-icons/md';
 const Search = () => {
     return (
         <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3 gap-4 px-4 py-16">
+            {/*Left side*/}
             <div className="lg:col-span-2 flex flex-col justify-evenly">
                 <div><h1>LUXURY INCLUDED VACATIONS FOR TWO PEOPLE</h1>
-                    <p> Come experience the very pinnacle of luxury Caribbean all-inclusive
+                    <p className="py-4"> Come experience the very pinnacle of luxury Caribbean all-inclusive
                         vacations for couples at BEACHES Resorts. Our luxury beach resorts,
                         set along the most gorgeous tropical settings and exquisite beaches
                         in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and
@@ -21,7 +22,7 @@ const Search = () => {
                 <div className="grid sm:grid-cols-2 gap-8 py-4">
                     <div className='flex flex-col lg:flex-row items-center text-center'>
                         <button>
-                            <RiCustomerService2Fill size={50} />
+                            <RiCustomerService2Fill  size={50} />
                         </button>
                         <div>
                             <h3 className='py-2'>LEADING SERVICE</h3>
@@ -39,7 +40,34 @@ const Search = () => {
                     </div>
                 </div>
             </div>
-            <div>right</div>
+            {/*Right side*/}
+            <div>
+                <div className='border text-center'>
+                    <p className='pt-2'>GET AN ADDITIONAL 10% OFF</p>
+                    <p className='py-4'>12 HOURS LEFT</p>
+                    <p className='bg-gray-800 text-gray-200 py-2'>BOOK NOW AND SAVE</p>
+                </div>
+                <form className='w-full'>
+                    <div className='flex flex-col my-2'>
+                        <label>Destination</label>
+                        <select className='border rounded-md p-2'>
+                            <option>Grande Antigua</option>
+                            <option>Key West</option>
+                            <option>Maldives</option>
+                            <option>Cozumel</option>
+                        </select>
+                    </div>
+                    <div className='flex flex-col my-4'>
+                        <label>Check-In</label>
+                        <input className='border rounded-md p-2' type="date" />
+                    </div>
+                    <div className='flex flex-col my-2'>
+                        <label>Check-Out</label>
+                        <input className='border rounded-md p-2' type="date" />
+                    </div>
+                    <button className='w-full my-4'>Rates & Availabilities</button>
+                </form>
+            </div>
         </div>
     );
 };
